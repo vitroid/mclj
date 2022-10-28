cbcd9982ced3199c6e02335b639315bf2aa34c6e
vitroid <vitroid@gmail.com>
2022-10-28 16:17:21 +0900

    表現を改良。

--- docs/cell.js
+++ docs/cell.js
@@ -1,33 +1,46 @@
-var pause=false
 
 function setup() {
     createCanvas(400, 400, WEBGL)
-    background(255,255,255)
+    background(200)
     noLoop()
+    normalMaterial()
+    cam = createCamera()
+    perspective(PI / 10.0, width / height, 0.1, 5000)
 }
 
 function draw() {
-    clear(255,255,255)
-    if ( pause )
-        return;
+    background(200)
 
+    cam.lookAt(0,0,0)
+    cam.setPosition(0,0,1000)
+    setCamera(cam)
     var cell=mclj.cell
+    scale(cell[0]*20)
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    stroke(0)
+    noFill()
+    box(1)
     noStroke()
-    shininess(1)
+    // shininess(0)
     fill(100,0,0)
+    ambientLight(100,100,100)
     ambientMaterial(255,0,0)
     pointLight(255,255,255,1000,-1000,1000)
-    specularMaterial(120)
-    scale(cell[0]*25)
+    specularMaterial(100)
     for(let i=0; i<mclj.pos.length; i++){
         let p = mclj.pos[i]
         push()
-        translate(p[0]-0.5, p[1]-0.5, p[2]-0.5)
+        p[0] -= floor(p[0]+0.5)
+        p[1] -= floor(p[1]+0.5)
+        p[2] -= floor(p[2]+0.5)
+        translate(p[0], p[1], p[2])
         sphere(sig0/cell[0]/10,4,4) //mclj.sig / cell[0])
         pop()
     }
 }
 
-function mousePressed(){
+function mousePressed(ev){
+    console.log(ev)
     console.log(mouseX, mouseY)
 }
